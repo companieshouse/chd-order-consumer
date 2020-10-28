c3178c81f175ce6933eb91c70a2a4d2424f21733
nabeelamirch <namir@companieshouse.gov.uk>
2020-10-28 11:42:19 +0000

    reverted code in start.sh

--- start.sh
+++ start.sh
@@ -27,6 +27,5 @@ else
     source "${APP_DIR}/app_env"
 fi
 
-#exec java ${JAVA_MEM_ARGS} -jar -Dserver.port="${PORT}" "${APP_DIR}/chd-order-consumer.jar"
-exec mvn spring-boot:run -Dspring-boot.run.jvmArguments="-Dserver.port=18577"
+exec java ${JAVA_MEM_ARGS} -jar -Dserver.port="${PORT}" "${APP_DIR}/chd-order-consumer.jar"
 
