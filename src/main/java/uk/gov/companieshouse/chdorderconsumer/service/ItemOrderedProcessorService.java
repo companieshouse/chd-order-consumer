e061fcf8fd0f9fa9643359c6eb486e56d2d139ee
ethatcher83 <ethatcher@companieshouse.gov.uk>
2020-11-06 10:12:58 +0000

    remove unused filing history id var

--- src/main/java/uk/gov/companieshouse/chdorderconsumer/service/ItemOrderedProcessorService.java
+++ src/main/java/uk/gov/companieshouse/chdorderconsumer/service/ItemOrderedProcessorService.java
@@ -32,8 +32,6 @@ public class ItemOrderedProcessorService {
         Item item = chdItemOrdered.getItem();
         Map<String, String> itemOptions = item.getItemOptions();
 
-        String filingHistoryId = itemOptions.get("filingHistoryId");
-
         missingImageDeliveryRequestApi.setId(item.getId());
         missingImageDeliveryRequestApi.setCompanyName(item.getCompanyName());
         missingImageDeliveryRequestApi.setCompanyNumber(item.getCompanyNumber());
