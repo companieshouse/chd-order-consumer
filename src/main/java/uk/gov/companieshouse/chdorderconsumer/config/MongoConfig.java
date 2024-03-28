92081a23b81324bbf78dff0c0632c40b09a72abe
vshastri-ch <vshastri@companieshouse.gov.uk>
2024-03-28 11:35:00 +0000

    kafka error changes

--- src/main/java/uk/gov/companieshouse/chdorderconsumer/config/MongoConfig.java
+++ src/main/java/uk/gov/companieshouse/chdorderconsumer/config/MongoConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MongoConfig {
 
-    @Value("${spring.data.mongodb.host}")
+    @Value("${spring.data.mongodb.uri}")
     private String connectionString;
 
     @Bean
