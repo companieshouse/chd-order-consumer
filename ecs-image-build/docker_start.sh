fee86e321fc62682ab1a124013cbadf822381c7c
sijegbulem-ch <sijegbulem@companieshouse.gov.uk>
2024-04-22 14:12:53 +0100

    trigger the ecs terraform release job and aaded permission to the docker.sh file

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #
 
+# Add execute permission to this script 
+chmod +x docker_start.sh
+
 # Start script for chd-order-consumer
 PORT=8080
 exec java -jar -Dserver.port="${PORT}" "chd-order-consumer.jar"
